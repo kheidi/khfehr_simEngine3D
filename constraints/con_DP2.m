630858eb54f902b98fcfdd6c364ca4d4a14d93cf
Katherine Fehr <katherine.heidi@gmail.com>
2020-10-11 12:37:54 -0500

    Update driver to test DP2

--- constraints/con_DP2.m
+++ constraints/con_DP2.m
@@ -1,11 +1,13 @@
-function results = con_DP2(p_i,p_i_dot,p_j,p_j_dot,a_i_bar,a_j_bar,s_i_P_bar,s_j_P_bar,r_i_dot,r_j_dot,f,df,ddf)
-% DP1DIRECT 
+function results = con_DP2(r_i,r_j,p_i,p_i_dot,p_j,p_j_dot,a_i_bar,a_j_bar,s_i_P_bar,s_j_P_bar,r_i_dot,r_j_dot,f,df,ddf,a_i_dot, a_j_dot)
+% CON_DP2 
 %
 % Author: K. Heidi Fehr
 % Email: kfehr@wisc.edu
 % October 2020
 %
 % TO-DO: 
+%   -add ability to only send in certain variables (varargin)
+%   -phi_p and phi_r might not be in the right size matrix
 
     %Phi
     A_i = getA(p_i);
@@ -28,9 +30,13 @@ function results = con_DP2(p_i,p_i_dot,p_j,p_j_dot,a_i_bar,a_j_bar,s_i_P_bar,s_j
         - 2*a_i_dot.'*dij_dot + ddf;
     
     %Phi_r
-
+    phi_r_i = -(a_i);
+    phi_r_j = a_i;
+    results.phi_r = [phi_r_i,phi_r_j];
     
     %Phi_p
-
+    phi_p_i = getB(p_i,a_i_bar).'*dij - getB(p_i,s_i_P_bar).'*a_i;
+    phi_p_j = getB(p_j,s_i_P_bar).'*a_i;
+    results.phi_p = [phi_p_i,phi_p_j];
     
 end
--- inputFiles/driver_gConSimple.m
+++ inputFiles/driver_gConSimple.m
@@ -19,6 +19,7 @@ p_i_dot(4) = -dot(p_i_dot,p_i)/p_i(4);
 p_i_dot = p_i_dot/norm(p_i_dot);
 
 a_i_bar = [-1.2;1;0.3];
+a_i_dot = [2;0;3];
 s_i_P_bar = [0.1;-0.3;6];
 
 r_j = [-0.5;1.6;-6.3];
@@ -31,6 +32,7 @@ p_j_dot(4) = -dot(p_j_dot,p_j)/p_j(4);
 p_j_dot = p_j_dot/norm(p_j_dot);
 
 a_j_bar = [1.2;4.5;3.1];
+a_j_dot = [1;3;0];
 s_j_P_bar = [0.2;-1;1.5];
 
 c = [0.3;0.4;-6];
@@ -41,34 +43,35 @@ ddf = 0.2;
 
 solDP1 = con_DP1(p_i,p_i_dot,p_j,p_j_dot,a_i_bar,a_j_bar,f,df,ddf)
 solCD = con_CD(r_i,p_i,p_i_dot,r_j,p_j,p_j_dot,s_i_P_bar,s_j_P_bar,c,f,df,ddf)
-
+solDP2 = con_DP2(r_i,r_j,p_i,p_i_dot,p_j,p_j_dot,a_i_bar,a_j_bar,s_i_P_bar,s_j_P_bar,r_i_dot,r_j_dot,f,df,ddf,a_i_dot, a_j_dot)
+solD = con_D(r_i,r_j,p_i,p_i_dot,p_j,p_j_dot,s_i_P_bar,s_j_P_bar,r_i_dot,r_j_dot,f,df,ddf)
 %% One Ground, One Body in Space
-clear
-
-r_i = [8;6;-3];
-p_i = [4;3;-5;1];
-p_i = p_i/norm(p_i);
-
-p_i_dot = [-0.2;1.3;3.4;0];
-p_i_dot(4) = -dot(p_i_dot,p_i)/p_i(4);
-p_i_dot = p_i_dot/norm(p_i_dot);
-
-a_i_bar = [-1.2;1;0.3];
-s_i_P_bar = [0.1;-0.3;6];
-
-r_j = [0;0;0];
-p_j = [0;0;0;0];
-
-p_j_dot = [0;0;0;0];
-
-a_j_bar = [0;0;0];
-s_j_P_bar = [0;0;0];
-
-c = [0.3;0.4;-6];
-
-f = 1.2;
-df = 2.5;
-ddf = 0.2;
-
-solDP1 = con_DP1(p_i,p_i_dot,p_j,p_j_dot,a_i_bar,a_j_bar,f,df,ddf)
-solCD = con_CD(r_i,p_i,p_i_dot,r_j,p_j,p_j_dot,s_i_P_bar,s_j_P_bar,c,f,df,ddf)
+% clear
+% 
+% r_i = [8;6;-3];
+% p_i = [4;3;-5;1];
+% p_i = p_i/norm(p_i);
+% 
+% p_i_dot = [-0.2;1.3;3.4;0];
+% p_i_dot(4) = -dot(p_i_dot,p_i)/p_i(4);
+% p_i_dot = p_i_dot/norm(p_i_dot);
+% 
+% a_i_bar = [-1.2;1;0.3];
+% s_i_P_bar = [0.1;-0.3;6];
+% 
+% r_j = [0;0;0];
+% p_j = [0;0;0;0];
+% 
+% p_j_dot = [0;0;0;0];
+% 
+% a_j_bar = [0;0;0];
+% s_j_P_bar = [0;0;0];
+% 
+% c = [0.3;0.4;-6];
+% 
+% f = 1.2;
+% df = 2.5;
+% ddf = 0.2;
+% 
+% solDP1 = con_DP1(p_i,p_i_dot,p_j,p_j_dot,a_i_bar,a_j_bar,f,df,ddf)
+% solCD = con_CD(r_i,p_i,p_i_dot,r_j,p_j,p_j_dot,s_i_P_bar,s_j_P_bar,c,f,df,ddf)
