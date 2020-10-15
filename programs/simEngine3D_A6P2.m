cd0b4eefdcf3469a31a6e2837479289befa2c45e
Katherine Fehr <katherine.heidi@gmail.com>
2020-10-15 00:05:02 -0500

    Newton Rhapson in for loop - Does not converge

--- programs/simEngine3D_A6P2.m
+++ programs/simEngine3D_A6P2.m
@@ -15,91 +15,97 @@ syms t;
 
 %% Guess Parameters
 guess.p_i = getEParams([0;0;0]);
-guess.p_j = getEParams([-90;-90;90+25]);
+guess.p_j = getEParams([-90;-90;90+45]);
 guess.r_i = [0;0;0];
-guess.r_j = [0;sqrt(2);-sqrt(2)];
-
-%% Constraints
-%%% Parallel-1
-clear data; data = guess;
-data.a_j_bar = [0;0;1]; %cj bar
-data.a_i_bar = [0;0;1]; %same as ai, Z axis of ground
-
-data.f = 0;
-con1 = con_DP1(data,'phi','phi_r','phi_p');
-
-data.a_j_bar = [0;0;1]; %cj bar
-data.a_i_bar = [0;1;0]; %sane as ai, Y axis of ground
-data.f = 0;
-con2 = con_DP1(data,'phi','phi_r','phi_p');
-
-%%% Spherical
-clear data; data = guess;
-data.r_i = [0;0;0];
-data.s_i_P_bar = [0;0;0]; %also s_i_P since A = I
-data.s_j_P_bar = [-L;0;0];
-data.f = 0;
-data.c = [1;0;0];
-con3 = con_CD(data,'phi','phi_r','phi_p');
-
-data.c = [0;1;0];
-con4 = con_CD(data,'phi','phi_r','phi_p');
-
-data.c = [0;0;1];
-con5 = con_CD(data,'phi','phi_r','phi_p');
-
-
-%%% Movement Function 
-% Driving Constraints
+guess.r_j = [0;2*sind(45);2*sind(45)];
 f = @(t) cosd((pi/4)*cosd(2*t)); %change to input
 df = matlabFunction( diff(f(t)) );
 ddf = matlabFunction( diff(df(t)) );
 
 T = 0; %change to input
-data.f = f(T);
-data.df = df(T);
-data.ddf = ddf(T);
+guess.f = f(T);
+guess.df = df(T);
+guess.ddf = ddf(T);
 
-data.a_i_bar = [0;0;-1]; %z axis of G-RF, this is what we want to set the angle with respect to
-data.a_j_bar = [1;0;0];
-con6 = con_DP1(data,'phi','phi_r','phi_p');
-
-%%% Euler Param Constraint
-con7.phi = data.p_j.'*data.p_j - 1;
-con7.phi_r = [0;0;0;0;0;0].';
-con7.phi_p = [2;2;2;2;2;2;2;2].';
-
-%% Calculations for Newton Rhapson
-
-%%% Current r-p
-rp = [guess.r_j;guess.p_j];
-
-%%% Build Phi_q
-% We only care about solving for the body j so we will only keep the
-% sections that pertain to it.
-phi_q = [
-    con1.phi_r(4:6),con1.phi_p(5:8);
-    con2.phi_r(4:6),con2.phi_p(5:8);
-    con3.phi_r(4:6),con3.phi_p(5:8);
-    con4.phi_r(4:6),con4.phi_p(5:8);
-    con5.phi_r(4:6),con5.phi_p(5:8);
-    con6.phi_r(4:6),con6.phi_p(5:8);
-    con7.phi_r(4:6),con7.phi_p(5:8)];
-
-%%% phi_Q
-% This is the collection of each phi value
-phi_Q = [
-    con1.phi;
-    con2.phi;
-    con3.phi;
-    con4.phi;
-    con5.phi;
-    con6.phi;
-    con7.phi];
+for i = 1:10
     
-%%% New r-p
-rp_next = rp-(phi_q*phi_Q)
+    %% Constraints
+    %%% Parallel-1
+    clear data; data = guess;
+    data.a_j_bar = [0;0;1]; %cj bar
+    data.a_i_bar = [0;0;1]; %same as ai, Z axis of ground
 
+    data.f = 0;
+    con1 = con_DP1(data,'phi','phi_r','phi_p');
+
+    data.a_j_bar = [0;0;1]; %cj bar
+    data.a_i_bar = [0;1;0]; %same as ai, Y axis of ground
+    data.f = 0;
+    con2 = con_DP1(data,'phi','phi_r','phi_p');
+
+    %%% Spherical
+    clear data; data = guess;
+    data.r_i = [0;0;0];
+    data.s_i_P_bar = [0;0;0]; %also s_i_P since A = I
+    data.s_j_P_bar = [-L;0;0];
+    data.f = 0;
+    data.c = [1;0;0];
+    con3 = con_CD(data,'phi','phi_r','phi_p');
+
+    data.c = [0;1;0];
+    con4 = con_CD(data,'phi','phi_r','phi_p');
+
+    data.c = [0;0;1];
+    con5 = con_CD(data,'phi','phi_r','phi_p');
+
+
+    %%% Movement Function 
+    % Driving Constraint
+
+    data.a_i_bar = [0;0;-1]; %z axis of G-RF, this is what we want to set the angle with respect to
+    data.a_j_bar = [1;0;0];
+    con6 = con_DP1(data,'phi','phi_r','phi_p');
+
+    %%% Euler Param Constraint
+    con7.phi = data.p_j.'*data.p_j - 1;
+    con7.phi_r = [0;0;0;0;0;0].';
+    con7.phi_p = [2;2;2;2;2;2;2;2].';
+
+    %% Calculations for Newton Rhapson
+
+    %%% Current r-p
+    rp = [guess.r_j;guess.p_j];
+
+    %%% Build Phi_q
+    % We only care about solving for the body j so we will only keep the
+    % sections that pertain to it.
+    phi_q = [
+        con1.phi_r(4:6),con1.phi_p(5:8);
+        con2.phi_r(4:6),con2.phi_p(5:8);
+        con3.phi_r(4:6),con3.phi_p(5:8);
+        con4.phi_r(4:6),con4.phi_p(5:8);
+        con5.phi_r(4:6),con5.phi_p(5:8);
+        con6.phi_r(4:6),con6.phi_p(5:8);
+        con7.phi_r(4:6),con7.phi_p(5:8)];
+
+    %%% phi_Q
+    % This is the collection of each phi value
+    phi_Q = [con1.phi;
+        con2.phi;
+        con3.phi;
+        con4.phi;
+        con5.phi;
+        con6.phi;
+        con7.phi];
+
+    %%% New r-p
+    rp_next = rp-(phi_q.'*phi_Q)
+    
+    guess.r_j = rp_next(1:3);
+    guess.p_j = rp_next(4:7);
+    
+
+end
 
 
 
