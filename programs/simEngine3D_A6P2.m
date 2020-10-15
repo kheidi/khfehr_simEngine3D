c722ec93cc3a4432f3c8155af2a75a845008cf32
Katherine Fehr <katherine.heidi@gmail.com>
2020-10-14 23:01:53 -0500

    Constraints run sans errors

--- programs/simEngine3D_A6P2.m
+++ programs/simEngine3D_A6P2.m
@@ -7,35 +7,43 @@
 %
 % TO-DO: 
 
-%% Guess Parameters
-guess.p_i = [0;0;0;0];
+%% Knowns
+L=2;
 
-%% Knowns and Constraints
+%% Guess Parameters
+guess.p_i = getEParams([0;0;0]);
+guess.p_j = getEParams([-90;-90;90+25]);
+guess.r_i = [0;0;0];
+guess.r_j = [0;sqrt(2);-sqrt(2)];
+
+%% Constraints
 %%% Parallel-1
 clear data; data = guess;
 data.a_j_bar = [0;0;1]; %cj bar
-data.a_i = [0;0;1];
+data.a_i_bar = [0;0;1]; %same as ai, Z axis of ground
+
 data.f = 0;
-con1 = con_DP1(data,'phi','nu','gamma','phi_r','phi_p');
+con1 = con_DP1(data,'phi','phi_r','phi_p');
 
 data.a_j_bar = [0;0;1]; %cj bar
-data.a_i = [0;1;0];
+data.a_i_bar = [0;1;0]; %sane as ai, Y axis of ground
 data.f = 0;
-con2 = con_DP1(data,'phi','nu','gamma','phi_r','phi_p');
+con2 = con_DP1(data,'phi','phi_r','phi_p');
 
 %%% Spherical
 clear data; data = guess;
 data.r_i = [0;0;0];
 data.s_i_P_bar = [0;0;0]; %also s_i_P since A = I
+data.s_j_P_bar = [-L;0;0];
 data.f = 0;
 data.c = [1;0;0];
-con3 = con_CD(data,'phi','nu','gamma','phi_r','phi_p');
+con3 = con_CD(data,'phi','phi_r','phi_p');
 
 data.c = [0;1;0];
-con4 = con_CD(data,'phi','nu','gamma','phi_r','phi_p');
+con4 = con_CD(data,'phi','phi_r','phi_p');
 
 data.c = [0;0;1];
-con5 = con_CD(data,'phi','nu','gamma','phi_r','phi_p');
+con5 = con_CD(data,'phi','phi_r','phi_p');
 
 
 %%% Movement Function 
@@ -51,10 +59,10 @@ data.ddf = ddf(T);
 
 data.a_i_bar = [0;0;-1]; %z axis of G-RF, this is what we want to set the angle with respect to
 data.a_j_bar = [1;0;0];
-con6 = con_DP1(data,'phi');
+con6 = con_DP1(data,'phi','phi_r','phi_p');
 
 %%% Euler Param Constraint
-con7 = p_j.'*p_j - 1;
+con7 = data.p_j.'*data.p_j - 1;
 
 
 
