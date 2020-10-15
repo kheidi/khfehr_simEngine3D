b5992fccc05b8974e0a9ca8ec8afdbc879091a13
Katherine Fehr <katherine.heidi@gmail.com>
2020-10-14 21:47:34 -0500

    Constraint 6&7

--- programs/simEngine3D_A6P2.m
+++ programs/simEngine3D_A6P2.m
@@ -1,16 +1,18 @@
-data.theta = @(t) (pi/4)*cosd(2*t);
-data.theta_dot = matlabFunction( diff(data.theta(t)) );
-data.theta_dot_dot = matlabFunction( diff(data.theta_dot(t)) );
+%% Pendulum
+% This program simulates a 3D pendulum with a movement of theta(t) =
+% (pi/4)*cos(2t)
+% Author: K. Heidi Fehr
+% Email: kfehr@wisc.edu
+% October 2020; Last revision: 14-Oct-2020
+%
+% TO-DO: 
 
-f = @(t) cosd((pi/4)*cosd(2*t));
-df = matlabFunction( diff(f(t)) );
-ddf = matlabFunction( diff(df(t)) );
-
-results = con_DP1(data,'phi')
+%% Guess Parameters
+guess.p_i = [0;0;0;0];
 
 %% Knowns and Constraints
-% Parallel-1
-clear data
+%%% Parallel-1
+clear data; data = guess;
 data.a_j_bar = [0;0;1]; %cj bar
 data.a_i = [0;0;1];
 data.f = 0;
@@ -21,8 +23,8 @@ data.a_i = [0;1;0];
 data.f = 0;
 con2 = con_DP1(data,'phi','nu','gamma','phi_r','phi_p');
 
-% Spherical
-clear data
+%%% Spherical
+clear data; data = guess;
 data.r_i = [0;0;0];
 data.s_i_P_bar = [0;0;0]; %also s_i_P since A = I
 data.f = 0;
@@ -34,6 +36,25 @@ con4 = con_CD(data,'phi','nu','gamma','phi_r','phi_p');
 
 data.c = [0;0;1];
 con5 = con_CD(data,'phi','nu','gamma','phi_r','phi_p');
+
+
+%%% Movement Function 
+% Driving Constraints
+f = @(t) cosd((pi/4)*cosd(2*t)); %change to input
+df = matlabFunction( diff(f(t)) );
+ddf = matlabFunction( diff(df(t)) );
+
+T = 0; %change to input
+data.f = f(T);
+data.df = df(T);
+data.ddf = ddf(T);
+
+data.a_i_bar = [0;0;-1]; %z axis of G-RF, this is what we want to set the angle with respect to
+data.a_j_bar = [1;0;0];
+con6 = con_DP1(data,'phi');
+
+%%% Euler Param Constraint
+con7 = p_j.'*p_j - 1;
 
 
 
