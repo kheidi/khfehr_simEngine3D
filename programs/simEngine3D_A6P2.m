89072883771a7c9bdcd57bea90b7fe21ef26d142
Katherine Fehr <katherine.heidi@gmail.com>
2020-10-21 17:27:31 -0500

    Fix velocity calculation

--- programs/simEngine3D_A6P2.m
+++ programs/simEngine3D_A6P2.m
@@ -13,17 +13,17 @@ function [phiResults,location] = simEngine3D_A6P2(T,previous)
     L=2;
     syms t;
     theta = @(t) (pi/4)*cos(2*t);
-    dtheta = matlabFunction( diff(theta(t)) );
-    ddtheta = matlabFunction( diff(dtheta(t)) );
-
+    dtheta = @(t)pi.*sin(t.*2.0).*(-1.0./2.0);
+    ddtheta = @(t)-pi.*cos(t.*2.0);
+    
     %% Guess Parameters
     guess.p_i = getEParams([0;0;0]);
     guess.p_j = previous;
     guess.r_i = [0;0;0];
     guess.r_j = [0;-1;-1];
     f = @(t) sin((pi/4)*cos(2*t));
-    df = matlabFunction( diff(f(t)) );
-    ddf = matlabFunction( diff(df(t)) );
+    df = @(t)pi.*sin(t.*2.0).*cos((pi.*cos(t.*2.0))./4.0).*(-1.0./2.0);
+    ddf = @(t)pi.^2.*sin(t.*2.0).^2.*sin((pi.*cos(t.*2.0))./4.0).*(-1.0./4.0)-pi.*cos(t.*2.0).*cos((pi.*cos(t.*2.0))./4.0);
 
     guess.f = f(T);
     guess.df = df(T);
@@ -43,14 +43,14 @@ function [phiResults,location] = simEngine3D_A6P2(T,previous)
         data.f = 0;
         data.df = 0;
         data.ddf = 0;
-        con1 = con_DP1(data,'phi','phi_r','phi_p');
+        con1 = con_DP1(data,'phi','phi_r','phi_p','nu');
 
         data.a_j_bar = [0;0;1]; %cj bar
         data.a_i_bar = [0;1;0]; %same as ai, Y axis of ground
         data.f = 0;
         data.df = 0;
         data.ddf = 0;
-        con2 = con_DP1(data,'phi','phi_r','phi_p');
+        con2 = con_DP1(data,'phi','phi_r','phi_p','nu');
 
         %%% Spherical
         clear data; data = guess;
@@ -61,13 +61,13 @@ function [phiResults,location] = simEngine3D_A6P2(T,previous)
         data.df = 0;
         data.ddf = 0;
         data.c = [1;0;0];
-        con3 = con_CD(data,'phi','phi_r','phi_p');
+        con3 = con_CD(data,'phi','phi_r','phi_p','nu');
 
         data.c = [0;1;0];
-        con4 = con_CD(data,'phi','phi_r','phi_p');
+        con4 = con_CD(data,'phi','phi_r','phi_p','nu');
 
         data.c = [0;0;1];
-        con5 = con_CD(data,'phi','phi_r','phi_p');
+        con5 = con_CD(data,'phi','phi_r','phi_p','nu');
 
 
         %%% Movement Function 
@@ -75,12 +75,13 @@ function [phiResults,location] = simEngine3D_A6P2(T,previous)
         clear data; data = guess;
         data.a_i_bar = [0;1;0]; %z axis of G-RF, this is what we want to set the angle with respect to
         data.a_j_bar = [1;0;0];
-        con6 = con_DP1(data,'phi','phi_r','phi_p');
+        con6 = con_DP1(data,'phi','phi_r','phi_p','nu');
 
         %%% Euler Param Constraint
         con7.phi = (data.p_j.'*data.p_j) - 1;
         con7.phi_r = [0;0;0;0;0;0].';
         con7.phi_p = 2*data.p_j.';
+        con7.nu = 0;
         %con7.phi_p = [2;2;2;2;2;2;2;2].';
 
         %% Calculations for Newton Rhapson
@@ -110,6 +111,18 @@ function [phiResults,location] = simEngine3D_A6P2(T,previous)
             con6.phi;
             con7.phi];
 
+        
+        %%% Find q_dot
+        nu_array = [
+            con1.nu;
+            con2.nu;
+            con3.nu;
+            con4.nu;
+            con5.nu;
+            con6.nu;
+            con7.nu];
+        q_dot = inv(phi_q)*nu_array;
+        
         %%% New r-p
         rp_next = rp-(inv(phi_q)*phi_Q);
         store(:,i) = rp_next;
@@ -119,6 +132,7 @@ function [phiResults,location] = simEngine3D_A6P2(T,previous)
         guess.p_j = rp_next(4:7);
 
         if i>50
+            disp('Did not converge in under 50 iterations')
             break
         end
 
@@ -132,18 +146,36 @@ function [phiResults,location] = simEngine3D_A6P2(T,previous)
 %     end
 
     %% Evaluate results
-    % 
+    
+    %%% Driving Constraint
     clear data; data = guess;
     data.a_i_bar = [0;1;0]; %z axis of G-RF, this is what we want to set the angle with respect to
     data.a_j_bar = [1;0;0];
     data.p_i_dot = getEParams([0;0;0]);
-    
-    %find p_j_dot (9.3.37)
-    omega = dtheta(T);
-    E = getE(data.p_j);
-    data.p_j_dot = 0.5*E.'*omega;
-    qdresults = con_DP1(data,'phi','phi_r','phi_p','gamma','nu');
-    phiResults = qdresults;
+ 
+    data.r_j_dot = q_dot(1:3);
+    data.p_j_dot = q_dot(4:7);
+    drivingConstraints = con_DP1(data,'phi','phi_r','phi_p','gamma','nu');
+    phiResults = drivingConstraints;
     location = rp_next;
+    
+%     %%% Parallel-1
+%     clear data; data = guess
+%     data.a_j_bar = [0;0;1]; %cj bar?
+%     data.a_i_bar = [0;0;1]; %same as ai, Z axis of ground
+% 
+%     data.f = 0;
+%     data.df = 0;
+%     data.ddf = 0;
+%     con1 = con_DP1(data,'phi','phi_r','phi_p');
+% 
+%     data.a_j_bar = [0;0;1]; %cj bar
+%     data.a_i_bar = [0;1;0]; %same as ai, Y axis of ground
+%     data.f = 0;
+%     data.df = 0;
+%     data.ddf = 0;
+%     con2 = con_DP1(data,'phi','phi_r','phi_p');
+%     
+    
 
 end
