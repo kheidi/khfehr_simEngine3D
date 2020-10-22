bb16e9e3d8a73faedb99d8a149454c99fa9e036b
Katherine Fehr <katherine.heidi@gmail.com>
2020-10-22 11:22:37 -0500

    Adds NR, not converging

--- programs/IVP_backwardEulerEx.m
+++ programs/IVP_backwardEulerEx.m
@@ -10,17 +10,31 @@
 
 %% KNOWNS
 h = 0.01
-x(1) = 0;
-y(1) = 2;
+x_array(1) = 0;
+y_array(1) = 2;
 t = linspace(0,20,1);
 
-%% FUNCTIONS
-% x = xn and y = yn
-g(1,1) = x*(1+h) + (4*h*x*y)/(1+x^2) - x_prev- h;
-g(2,1) = -h*x + y+(h*x*y)/(1+x^2) - y_prev;
+x_prev = 0;
+y_prev = 0;
+%% NR Solve
+for i = 1:500
+    x = x_array(i);
+    y = y_array(i);
+    %% FUNCTIONS
+    % x = xn and y = yn
+    g(1,1) = x*(1+h) + (4*h*x*y)/(1+x^2) - x_prev- h;
+    g(2,1) = -h*x + y+(h*x*y)/(1+x^2) - y_prev;
 
-% Jacobian:
-J(1,1) = 1+h+4*h*y*((1-x^2)/((1+x^2)^2));
-J(1,2) = (4*h*x)/(1+x^2);
-J(2,1) = -h + h*y*((1-x^2)/((1+x^2)^2));
-J(2,2) = 1 + ((h*x)/(1+x^2));
+    % Jacobian:
+    J(1,1) = 1+h+4*h*y*((1-x^2)/((1+x^2)^2));
+    J(1,2) = (4*h*x)/(1+x^2);
+    J(2,1) = -h + h*y*((1-x^2)/((1+x^2)^2));
+    J(2,2) = 1 + ((h*x)/(1+x^2));
+
+    delta = inv(J)*-g
+    x_array(i+1) = x+delta(1);
+    y_array(i+1) = y+delta(2);
+    x_prev = x;
+    y_prev = y;
+end
+    
