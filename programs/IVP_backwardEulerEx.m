463f0da04f5810e59ef8cd3a2bfe165586ae446a
Katherine Fehr <katherine.heidi@gmail.com>
2020-10-22 19:55:27 -0500

    Rearrange functions

--- programs/IVP_backwardEulerEx.m
+++ programs/IVP_backwardEulerEx.m
@@ -9,27 +9,31 @@
 %   -
 
 %% KNOWNS
-h = 0.01
+h = 0.01;
 x_array(1) = 0;
 y_array(1) = 2;
-t = linspace(0,20,1);
+t0 = 0;
+tf = 20;
+time = t0:h:tf;
 
 x_prev = 0;
-y_prev = 0;
+y_prev = 2;
+
+%% EQUATIONS
+% Provided:
+f = @(x,y) 1 - x - (4*x*y)/(1+x^2);
+g = @(x,y) x(1-(y/(1+x^2)));
+
+% Jacobian:
+J_f_x = 1+h+4*h*y*((1-x^2)/((1+x^2)^2));
+J_f_y = (4*h*x)/(1+x^2);
+J_g_x = -h + h*y*((1-x^2)/((1+x^2)^2));
+J_g_y = 1 + ((h*x)/(1+x^2)); 
+
 %% NR Solve
 for i = 1:500
     x = x_array(i);
     y = y_array(i);
-    %% FUNCTIONS
-    % x = xn and y = yn
-    g(1,1) = x*(1+h) + (4*h*x*y)/(1+x^2) - x_prev- h;
-    g(2,1) = -h*x + y+(h*x*y)/(1+x^2) - y_prev;
-
-    % Jacobian:
-    J(1,1) = 1+h+4*h*y*((1-x^2)/((1+x^2)^2));
-    J(1,2) = (4*h*x)/(1+x^2);
-    J(2,1) = -h + h*y*((1-x^2)/((1+x^2)^2));
-    J(2,2) = 1 + ((h*x)/(1+x^2));
 
     delta = inv(J)*-g
     x_array(i+1) = x+delta(1);
@@ -37,4 +41,7 @@ for i = 1:500
     x_prev = x;
     y_prev = y;
 end
+
+figure;
+plot(x_array)
     
