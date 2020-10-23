ca782d861a99177970b9fedd2e36b42cec5cff79
Katherine Fehr <katherine.heidi@gmail.com>
2020-10-22 20:35:45 -0500

    Can find delta!!

--- programs/IVP_backwardEulerEx.m
+++ programs/IVP_backwardEulerEx.m
@@ -16,22 +16,64 @@ t0 = 0;
 tf = 20;
 time = t0:h:tf;
 
-x_prev = 0;
-y_prev = 2;
-
 %% EQUATIONS
 % Provided:
 f = @(x,y) 1 - x - (4*x*y)/(1+x^2);
-g = @(x,y) x(1-(y/(1+x^2)));
+g = @(x,y) x*(1-(y/(1+x^2)));
 
 % Jacobian:
-J_f_x = 1+h+4*h*y*((1-x^2)/((1+x^2)^2));
-J_f_y = (4*h*x)/(1+x^2);
-J_g_x = -h + h*y*((1-x^2)/((1+x^2)^2));
-J_g_y = 1 + ((h*x)/(1+x^2)); 
+J_f_x = @(x,y) 1+h+4*h*y*((1-x^2)/((1+x^2)^2));
+J_f_y = @(x,y) (4*h*x)/(1+x^2);
+J_g_x = @(x,y) -h + h*y*((1-x^2)/((1+x^2)^2));
+J_g_y = @(x,y) 1 + ((h*x)/(1+x^2)); 
 
 %% NR Solve
-for i = 1:500
+% Initialize:
+f_prev = zeros(1,length(time))
+g_prev = zeros(1,length(time))
+
+x_prev = 0;
+y_prev = 2;
+
+c = 1; %count
+
+for i = 2:length(time)
+    
+    
+    f_first(1,i) = f_prev(1,i-1) + h*f(f_prev(1,i-1),g_prev(1,i-1));
+    g_first(1,i) = g_prev(1,i-1) + h*f(f_prev(1,i-1),g_prev(1,i-1));
+    
+    %Evaulate at current
+    f_eval = f(f_first(1,i),g_first(1,i))
+    g_eval = g(f_first(1,i),g_first(1,i))
+    
+    %First guess
+    f_new(1,1) = f_prev(1,i-1) + h*f_eval;
+    g_new(1,1) = g_prev(1,i-1) + h*g_eval;
+    
+    while error > 0.001
+        f_eval = f(f_new(1,c),g_new(1,c));
+        g_eval = g(f_new(1,c),g_new(1,c));
+        
+        %find delta
+        fg = [ 
+            f_new(1,c) - f_prev(1,i-1) - h*f_eval;
+            g_new(1,c) - g_prev(1,i-1) - h*g_eval];
+        J = [
+            J_f_x(f_new(1,c),g_new(1,c)),J_f_y(f_new(1,c),g_new(1,c));
+            J_g_x(f_new(1,c),g_new(1,c)),J_g_y(f_new(1,c),g_new(1,c))]
+        delta = J\-fg;
+            
+        
+        if c > 30
+            break
+        end
+    end
+    
+    c = c + 1;
+    
+    
+    
     x = x_array(i);
     y = y_array(i);
 
