4e1623b009af03e2cd1302df415cbfe9cd9c9124
Katherine Fehr <katherine.heidi@gmail.com>
2020-10-28 16:44:27 -0500

    Starts dynamicAnalysis

---
+++ programs/dynamicsAnalysis.m
@@ -0,0 +1,13 @@
+function results = dynamicsAnalysis(x, minus1, minus2)
+% Based on Lecture17 slide 8 steps
+
+%%% Step 0: Seed with 1st order BDF
+
+if orderNum == 1
+    % From table:
+    beta0 = 1;
+    alpha0 = 1;
+    alpha1 = -1;
+    C_r = alpha1 * minus1.r_dot
+
+end
