3b22cb6acaf6e71fce64e29b74bf3e895caf060e
Katherine Fehr <katherine.heidi@gmail.com>
2020-11-11 16:40:28 -0600

    Corrected to use Gaussian Quadrature

--- HW10/FMBD2.m
+++ HW10/FMBD2.m
@@ -2,63 +2,7 @@
 % Author: K. Heidi Fehr, Date: 11/05/2020
 
 %% Knowns
-globalZero = [0;0;0]; % Center of beam, (u,v,w)
-L = 0.5; %m
-H = 0.003; %m
-W = 0.003; %m
-g = [0;0;-9.81]; %m/s
-youngsM = 2e11; %Pa
-rho = 7700; % density, kg/m^3
-nu = 0.3; %poissons ratio
-k1 = 10*((1+nu)/(12+11*nu));
-k2 = k1;
 
-%% Initial calculations
-r1 = [0;0;0]; %r(j)
-r2 = [0.5;0;0]; %r(j+1)
-
-% (u,v,w)
-p1 = [-0.25;0;0]; %r(j)
-r2 = [0.25;0;0]; %r(j+1)
-
-
-%%% Part A) Shape function in normalized coordinates
-syms xi eta zeta
-% Slide 22-38
-S_xiXi(1) = (3/4)*(xi^2-1);
-S_xiXi(2) = (L/8)*(3*xi^2-2*xi-1);
-S_xiXi(3) = -(H*eta)/4;
-S_xiXi(4) = -(W*zeta)/4;
-S_xiXi(5) = (3/4)*((-xi^2) + 1);
-S_xiXi(6) = (L/8)*(3*(xi^2) + 2*xi - 1);
-S_xiXi(7) = H*eta/4;
-S_xiXi(8) = W*zeta/4;
-
-S_xiXi_all = [
-    S_xiXi(1)*eye(3);
-    S_xiXi(2)*eye(3);
-    S_xiXi(3)*eye(3);
-    S_xiXi(4)*eye(3);
-    S_xiXi(5)*eye(3);
-    S_xiXi(6)*eye(3);
-    S_xiXi(7)*eye(3);
-    S_xiXi(8)*eye(3)].';
-fprintf('Normalized Coordinates Shape Function with respect to Xi:\n\n')
-disp(S_xiXi(1));
-disp(S_xiXi(2));
-disp(S_xiXi(3));
-disp(S_xiXi(4));
-disp(S_xiXi(5));
-disp(S_xiXi(6));
-disp(S_xiXi(7));
-disp(S_xiXi(8));
-
-
-
-%%% Part B)
-
-% e0  
-% Undeformed initial position:
 clear
 L = 0.5; %m
 H = 0.003; %m
@@ -72,6 +16,7 @@ k1 = 10*((1+nu)/(12+11*nu));
 k2 = k1;
 k3 = k2;
 
+% Undeformed initial condition
 r1 = [0;0;0]; %r(j)
 r2 = [0.5;0;0]; %r(j+1)
 r1_u = [1;0;0];
@@ -85,158 +30,28 @@ e1 = [r1;r1_u;r1_j;r1_w];
 e2 = [r2;r2_u;r2_j;r2_w];
 e0 = [e1;e2];
 
-%Shape functions, but symbolic:
-syms xi eta zeta
-% Slide 22-38
-Sym_xi(1) = (1/4)*((xi^3)-(3*xi)+2);
-Sym_xi(2) = (L/8)*((xi^3)-(xi^2)-(xi)+1);
-Sym_xi(3) = ((H*eta)/4)*(-xi+1);
-Sym_xi(4) = ((W*zeta)/4)*(-xi+1);
-Sym_xi(5) = (1/4)*((-xi^3)+(3*xi)+2);
-Sym_xi(6) = (L/8)*((xi^3)+(xi^2)-(xi)-1);
-Sym_xi(7) = ((H*eta)/4)*(xi+1);
-Sym_xi(8) = ((W*zeta)/4)*(xi+1);
-
-Sym_xi_all = [
-    Sym_xi(1)*eye(3);
-    Sym_xi(2)*eye(3);
-    Sym_xi(3)*eye(3);
-    Sym_xi(4)*eye(3);
-    Sym_xi(5)*eye(3);
-    Sym_xi(6)*eye(3);
-    Sym_xi(7)*eye(3);
-    Sym_xi(8)*eye(3)].';
-
-Sym_xiXi(1) = (3/4)*((xi^2)-1);
-Sym_xiXi(2) = (L/8)*(3*(xi^2)-2*xi-1);
-Sym_xiXi(3) = -(H*eta)/4;
-Sym_xiXi(4) = -(W*zeta)/4;
-Sym_xiXi(5) = (3/4)*((-xi^2) + 1);
-Sym_xiXi(6) = (L/8)*(3*(xi^2) + 2*xi - 1);
-Sym_xiXi(7) = H*eta/4;
-Sym_xiXi(8) = W*zeta/4;
-
-Sym_xiXi_all = [
-    Sym_xiXi(1)*eye(3);
-    Sym_xiXi(2)*eye(3);
-    Sym_xiXi(3)*eye(3);
-    Sym_xiXi(4)*eye(3);
-    Sym_xiXi(5)*eye(3);
-    Sym_xiXi(6)*eye(3);
-    Sym_xiXi(7)*eye(3);
-    Sym_xiXi(8)*eye(3)].';
-
-Sym_xiEta = sym('Sym_xiEta', [3 24]);
-
-Sym_xiEta(1) = 0;
-Sym_xiEta(2) = 0;
-Sym_xiEta(3) = (H/4)*(-xi+1);
-Sym_xiEta(4) = 0;
-Sym_xiEta(5) = 0;
-Sym_xiEta(6) = 0;
-Sym_xiEta(7) = (H/4)*(xi+1);
-Sym_xiEta(8) = 0;
-
-Sym_xiEta_all = [
-    Sym_xiEta(1)*eye(3);
-    Sym_xiEta(2)*eye(3);
-    Sym_xiEta(3)*eye(3);
-    Sym_xiEta(4)*eye(3);
-    Sym_xiEta(5)*eye(3);
-    Sym_xiEta(6)*eye(3);
-    Sym_xiEta(7)*eye(3);
-    Sym_xiEta(8)*eye(3)].';
-
-Sym_xiZeta = sym('Sym_xiZeta', [3 24]);
-
-Sym_xiZeta(1) = 0;
-Sym_xiZeta(2) = 0;
-Sym_xiZeta(3) = 0;
-Sym_xiZeta(4) = (W/4)*(-xi+1);
-Sym_xiZeta(5) = 0;
-Sym_xiZeta(6) = 0;
-Sym_xiZeta(7) = 0;
-Sym_xiZeta(8) = (W/4)*(xi+1);
-
-Sym_xiZeta_all = [
-    Sym_xiZeta(1)*eye(3);
-    Sym_xiZeta(2)*eye(3);
-    Sym_xiZeta(3)*eye(3);
-    Sym_xiZeta(4)*eye(3);
-    Sym_xiZeta(5)*eye(3);
-    Sym_xiZeta(6)*eye(3);
-    Sym_xiZeta(7)*eye(3);
-    Sym_xiZeta(8)*eye(3)].';
-
-clear Sym_xiXi_all Sym_xiEta_all Sym_xiZeta_all
-Sym_xiXi_all = diff(Sym_xi_all,xi);
-
-Sym_xiEta_all = diff(Sym_xi_all,eta);
-
-Sym_xiZeta_all = diff(Sym_xi_all,zeta);
-
-% Matrix to be normalized:
-J0 = [Sym_xiXi_all*e0, Sym_xiEta_all*e0, Sym_xiZeta_all*e0];
-detJ = det(J0);
-% Expression to integrate:
-% limits
-% rho = 1;
-a = -1;
-b = 1;
-f = rho*(Sym_xi_all.')*Sym_xi_all*detJ;
-% integrals
-M = int(f,zeta,a,b);
-M = int(M,eta,a,b);
-M = int(M,xi,a,b);
+GQPoints = [6,2,2];
+M = getMass(L,H,W,rho,e0,GQPoints);
 
 fprintf('Diagonal terms of M:\n\n')
 disp(vpa(diag(M)));
 
-%%% Part C) Generalized Force Vector due to Gravity
+%% Part C) Generalized Force Vector due to Gravity
 
 % Function slide 21-35
+GQPoints = [6,2,2];
 F_grav = g;
-f = rho*(Sym_xi_all.')*F_grav*detJ;
-a = -1;
-b = 1;
-% integrals
-Q_g = int(f,zeta,a,b);
-Q_g = int(Q_g,eta,a,b);
-Q_g = int(Q_g,xi,a,b);
+Q_g = getQ_g(L,H,W,rho,e0,F_grav,GQPoints);
 
 fprintf('Generalized force vector due to gravity:\n\n')
 disp(vpa(Q_g));
 
-%%% Part D) Internal Force Vector
+%% Part D) Internal Force Vector
 % Given in the exercise:
 e = [0,0,0,1,0,0,0,1,0,0,0,1,...
     0.5,0,0,...
     0,0,-1,...
     0,1,0,1,0,0].';
-%Slide 22-37
-r = Sym_xi_all*e;
-r0 = Sym_xi_all*e0;
-
-%Deformation Gradient Tensor, 22-39
-F_e = [Sym_xiXi_all*e, Sym_xiEta_all*e, Sym_xiZeta_all*e]*inv(J0);
-Jinv = inv(J0);
-
-SF1 = (Jinv(1,1)*Sym_xiXi_all + Jinv(2,1)*Sym_xiEta_all + Jinv(3,1)*Sym_xiZeta_all);
-SF2 = (Jinv(1,2)*Sym_xiXi_all + Jinv(2,2)*Sym_xiEta_all + Jinv(3,2)*Sym_xiZeta_all);
-SF3 = (Jinv(1,3)*Sym_xiXi_all + Jinv(2,3)*Sym_xiEta_all + Jinv(3,3)*Sym_xiZeta_all);
-SF{1} = SF1;
-SF{2} = SF2;
-SF{3} = SF3;
-
-%Cauchy-Green Deformation Tensor 22-40
-C = F_e.'*F_e;
-
-%Green-Lagrange Strain Tensor, 22-41
-E = 0.5*(C-eye(3));
-%E in vector form:
-epsilon = [E(1,1), E(2,2), E(3,3), 2*E(2,3), 2*E(1,3), 2*E(1,2)].';
-
-%Elasticity Matrix, 22-46
 D = ((youngsM*nu)/((1+nu)*(1-2*nu)))*...
     [
     (1-nu)/nu,1,1,0,0,0;
@@ -245,44 +60,16 @@ D = ((youngsM*nu)/((1+nu)*(1-2*nu)))*...
     0,0,0,(1-2*nu)/(2*nu),0,0;
     0,0,0,0,((1-2*nu)/(2*nu))*k2,0;
     0,0,0,0,0,((1-2*nu)/(2*nu))*k3];
-
-%Strain Energy - U Linear Hookean, 22-44
-f = epsilon.'*D*epsilon;
-a = -1;
-b = 1;
-% integrals
-ULH = int(f,zeta,a,b);
-ULH= int(ULH,eta,a,b);
-ULH = 0.5*int(ULH,xi,a,b);
-
-%Q Linear Lookean, 22-48
-% Set up the first part thats a double summation:
-part1 = sym(1);
-for i = 1:3
-    for j = 1:3
-        part1 = part1 + (SF{i}.'*SF{i} *e)*((D(i,j)/2)*(e.'*SF{j}.'*SF{j}*e-1));
-    end
-end
-% Set up part 2 which is everything but that double summation:
-part2 = ((SF2.'*SF3+SF3.'*SF2)*e) * (D(4,4)*(e.'*SF2.'*SF3*e))...
-    + ((SF1.'*SF3+SF3.'*SF1)*e) * (D(5,5)*(e.'*SF1.'*SF3*e))...
-    + ((SF1.'*SF2+SF2.'*SF1)*e) * (D(6,6)*(e.'*SF1.'*SF2*e));
-% integrals
-f = (part1+part2)*detJ;
-a = -1;
-b = 1;
-% integrals
-Q_intLH = int(f,zeta,a,b);
-Q_intLH= int(Q_intLH,eta,a,b);
-Q_intLH = (-1)*int(Q_intLH,xi,a,b);
+GQPoints = [5,3,3];
+Q_intLH = getQ_int(L,H,W,rho,e0,e,D,GQPoints);
 
 fprintf('Generalized internal force vector:\n\n')
 disp(vpa(Q_intLH));
 
-%%% Part E) Global position of points & plot
+%% Part E) Global position of points & plot
 syms xi eta zeta
 % Function to calculate positions of points from Xi and nodal coordinates
-Sym_r_p = Sym_xi_all*e; % e contains nodal coordinate 
+Sym_r_p = getS_xi(xi,eta,zeta,L,H,W)*e; % e contains nodal coordinate 
 xi_ = -1:0.1:1; % Xi
 for i = 1:length(xi_)
     r_p(i,:) = double(subs(Sym_r_p, [xi,eta,zeta], [xi_(i),0,0]));
@@ -293,10 +80,10 @@ plot(r_p(:,1),r_p(:,3))
 title('Two-Node Beam in at New Global Coordinates')
 axis equal
 
-%%% Part F) Gen. force vector due to external point force
+%% Part F) Gen. force vector due to external point force
 pointXi = [1,0,0]; %TBD
 force_applied = [10*cosd(45);0;10*sind(45)]; %N, provided
-shapePoint = double(subs(Sym_xi_all, [xi,eta,zeta], [1,0,0]));
+shapePoint = double(subs(getS_xi(xi,eta,zeta,L,H,W), [xi,eta,zeta], [1,0,0]));
 %Slide 24-20
 Q_ext = shapePoint.'*force_applied;
 fprintf('Generalized force vector due to external point force:\n\n')
@@ -383,13 +170,193 @@ elseif n == 6
 end
 end
 
+function S_xi_all = getS_xi(xi,eta,zeta,L,H,W)
+S_xi(1) = (1/4)*((xi^3)-(3*xi)+2);
+S_xi(2) = (L/8)*((xi^3)-(xi^2)-(xi)+1);
+S_xi(3) = ((H*eta)/4)*(-xi+1);
+S_xi(4) = ((W*zeta)/4)*(-xi+1);
+S_xi(5) = (1/4)*((-xi^3)+(3*xi)+2);
+S_xi(6) = (L/8)*((xi^3)+(xi^2)-(xi)-1);
+S_xi(7) = ((H*eta)/4)*(xi+1);
+S_xi(8) = ((W*zeta)/4)*(xi+1);
 
+S_xi_all = [
+    S_xi(1)*eye(3);
+    S_xi(2)*eye(3);
+    S_xi(3)*eye(3);
+    S_xi(4)*eye(3);
+    S_xi(5)*eye(3);
+    S_xi(6)*eye(3);
+    S_xi(7)*eye(3);
+    S_xi(8)*eye(3)].';
+end
+
+function S_xiXi_all = getS_xiXi(xi,eta,zeta,L,H,W)
+S_xiXi(1) = (3/4)*((xi^2)-1);
+S_xiXi(2) = (L/8)*(3*(xi^2)-2*xi-1);
+S_xiXi(3) = -(H*eta)/4;
+S_xiXi(4) = -(W*zeta)/4;
+S_xiXi(5) = (3/4)*((-xi^2) + 1);
+S_xiXi(6) = (L/8)*(3*(xi^2) + 2*xi - 1);
+S_xiXi(7) = H*eta/4;
+S_xiXi(8) = W*zeta/4;
+
+S_xiXi_all = [
+    S_xiXi(1)*eye(3);
+    S_xiXi(2)*eye(3);
+    S_xiXi(3)*eye(3);
+    S_xiXi(4)*eye(3);
+    S_xiXi(5)*eye(3);
+    S_xiXi(6)*eye(3);
+    S_xiXi(7)*eye(3);
+    S_xiXi(8)*eye(3)].';
+end
+ 
+function S_xiEta_all = getS_xiEta(xi,eta,zeta,L,H,W)
+% S_xiEta = sym('Sym_xiEta', [3 24]);
+
+S_xiEta(1) = 0;
+S_xiEta(2) = 0;
+S_xiEta(3) = (H/4)*(-xi+1);
+S_xiEta(4) = 0;
+S_xiEta(5) = 0;
+S_xiEta(6) = 0;
+S_xiEta(7) = (H/4)*(xi+1);
+S_xiEta(8) = 0;
+
+S_xiEta_all = [
+    S_xiEta(1)*eye(3);
+    S_xiEta(2)*eye(3);
+    S_xiEta(3)*eye(3);
+    S_xiEta(4)*eye(3);
+    S_xiEta(5)*eye(3);
+    S_xiEta(6)*eye(3);
+    S_xiEta(7)*eye(3);
+    S_xiEta(8)*eye(3)].';
+end
+
+function S_xiZeta_all = getS_xiZeta(xi,eta,zeta,L,H,W)
+
+% S_xiZeta = sym('Sym_xiZeta', [3 24]);
+
+S_xiZeta(1) = 0;
+S_xiZeta(2) = 0;
+S_xiZeta(3) = 0;
+S_xiZeta(4) = (W/4)*(-xi+1);
+S_xiZeta(5) = 0;
+S_xiZeta(6) = 0;
+S_xiZeta(7) = 0;
+S_xiZeta(8) = (W/4)*(xi+1);
+
+S_xiZeta_all = [
+    S_xiZeta(1)*eye(3);
+    S_xiZeta(2)*eye(3);
+    S_xiZeta(3)*eye(3);
+    S_xiZeta(4)*eye(3);
+    S_xiZeta(5)*eye(3);
+    S_xiZeta(6)*eye(3);
+    S_xiZeta(7)*eye(3);
+    S_xiZeta(8)*eye(3)].';
+end
     
+function M = getMass(L,H,W,rho,e0,GQPoints)
+M = 0
+[W_xi P_xi] = GQValues(GQPoints(1));
+[W_eta P_eta] = GQValues(GQPoints(1));
+[W_zeta P_zeta] = GQValues(GQPoints(1));
+
+for i = 1:length(P_xi)
+    for j = 1:length(P_eta)
+        for k = 1:length(P_zeta)
+            xi = P_xi(i);
+            eta = P_eta(j);
+            zeta = P_zeta(k);
+            %Jacobian
+            J0 = [getS_xiXi(xi,eta,zeta,L,H,W)*e0, getS_xiEta(xi,eta,zeta,L,H,W)*e0, getS_xiZeta(xi,eta,zeta,L,H,W)*e0];
+            detJ = det(J0);
+            %Find Mass Matrix
+            M = M + rho*(getS_xi(xi,eta,zeta,L,H,W).')*getS_xi(xi,eta,zeta,L,H,W)*detJ*W_xi(i)*W_eta(j)*W_zeta(k);
+        end
+    end
+end
     
+end
+
+function Q_g = getQ_g(L,H,W,rho,e0,F_grav,GQPoints)
+Q_g = 0;
+[W_xi P_xi] = GQValues(GQPoints(1));
+[W_eta P_eta] = GQValues(GQPoints(1));
+[W_zeta P_zeta] = GQValues(GQPoints(1));
+
+for i = 1:length(P_xi)
+    for j = 1:length(P_eta)
+        for k = 1:length(P_zeta)
+            xi = P_xi(i);
+            eta = P_eta(j);
+            zeta = P_zeta(k);
+            %Jacobian
+            J0 = [getS_xiXi(xi,eta,zeta,L,H,W)*e0, getS_xiEta(xi,eta,zeta,L,H,W)*e0, getS_xiZeta(xi,eta,zeta,L,H,W)*e0];
+            detJ = det(J0);
+            %Find Mass Matrix
+            Q_g = Q_g + rho*(getS_xi(xi,eta,zeta,L,H,W).')*F_grav*detJ*W_xi(i)*W_eta(j)*W_zeta(k);
+        end
+    end
+end
     
-    
-    
-    
-    
-    
-    
+end
+function SF = getSF(xi,eta,zeta,L,W,H,e0)
+
+J0 = [getS_xiXi(xi,eta,zeta,L,H,W)*e0, getS_xiEta(xi,eta,zeta,L,H,W)*e0, getS_xiZeta(xi,eta,zeta,L,H,W)*e0];
+Jinv = inv(J0);
+
+S_xiXi_all = getS_xiXi(xi,eta,zeta,L,H,W);
+S_xiEta_all = getS_xiEta(xi,eta,zeta,L,H,W);
+S_xiZeta_all = getS_xiZeta(xi,eta,zeta,L,H,W);
+
+SF1 = (Jinv(1,1)*S_xiXi_all + Jinv(2,1)*S_xiEta_all + Jinv(3,1)*S_xiZeta_all);
+SF2 = (Jinv(1,2)*S_xiXi_all + Jinv(2,2)*S_xiEta_all + Jinv(3,2)*S_xiZeta_all);
+SF3 = (Jinv(1,3)*S_xiXi_all + Jinv(2,3)*S_xiEta_all + Jinv(3,3)*S_xiZeta_all);
+SF{1} = SF1;
+SF{2} = SF2;
+SF{3} = SF3;
+
+end
+
+function Q_int = getQ_int(L,H,W,rho,e0,e,D,GQPoints)
+Q_int = 0;
+[W_xi P_xi] = GQValues(GQPoints(1));
+[W_eta P_eta] = GQValues(GQPoints(1));
+[W_zeta P_zeta] = GQValues(GQPoints(1));
+
+for i = 1:length(P_xi)
+    for j = 1:length(P_eta)
+        for k = 1:length(P_zeta)
+            xi = P_xi(i);
+            eta = P_eta(j);
+            zeta = P_zeta(k);
+            %Jacobian
+            J0 = [getS_xiXi(xi,eta,zeta,L,H,W)*e0, getS_xiEta(xi,eta,zeta,L,H,W)*e0, getS_xiZeta(xi,eta,zeta,L,H,W)*e0];
+            detJ = det(J0);
+            
+            SF = getSF(xi,eta,zeta,L,W,H,e0);
+            SF1 = SF{1};
+            SF2 = SF{2};
+            SF3 = SF{3};
+            
+            part1 = 0;
+            for m = 1:3
+                for n = 1:3
+                    part1 = part1 + (SF{m}.'*SF{m} *e)*((D(m,n)/2)*(e.'*SF{n}.'*SF{n}*e-1));
+                end
+            end
+            part2 = ((SF2.'*SF3+SF3.'*SF2)*e) * (D(4,4)*(e.'*SF2.'*SF3*e))...
+                + ((SF1.'*SF3+SF3.'*SF1)*e) * (D(5,5)*(e.'*SF1.'*SF3*e))...
+                + ((SF1.'*SF2+SF2.'*SF1)*e) * (D(6,6)*(e.'*SF1.'*SF2*e));
+            
+            %Find Mass Matrix
+            Q_int = Q_int + (part1+part2)*detJ*W_xi(i)*W_eta(j)*W_zeta(k);
+        end
+    end
+end
+Q_int = -1*Q_int;  
+end
