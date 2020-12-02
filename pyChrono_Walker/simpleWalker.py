38ed0a0293dbdb2a081072d4baee684b916f6ccd
Katherine Fehr <kfehr@wisc.edu>
2020-12-02 15:34:38 -0600

    Starts simpleWalker with base from sphereRoll

---
+++ pyChrono_Walker/simpleWalker.py
@@ -0,0 +1,90 @@
+#------------------------------------------------------------------------------
+# Name:        Simple Walker 2D
+# Purpose:     Implementing the simpliest walking model in 2D 
+#
+# Author:      Katherine Heidi Fehr
+#
+# Created:     12/1/2020
+#------------------------------------------------------------------------------
+
+import pychrono.core as chrono
+import pychrono.irrlicht as chronoirr
+
+# ---------------------------------------------------------------------
+#
+#  Create the simulation system and add items
+#
+
+mysystem      = chrono.ChSystemNSC()
+
+
+
+# Set the global collision margins. This is expecially important for very large or
+# very small objects. Set this before creating shapes. Not before creating mysystem.
+chrono.ChCollisionModel.SetDefaultSuggestedEnvelope(0.001);
+chrono.ChCollisionModel.SetDefaultSuggestedMargin(0.001);
+
+# ---------------------------------------------------------------------
+#
+#  Create the simulation system and add items
+#
+
+# Create a contact material (with default properties, shared by all collision shapes)
+contact_material = chrono.ChMaterialSurfaceNSC()
+contact_material.SetFriction(.1)
+contact_material.SetDampingF(0.2)
+contact_material.SetCompliance (0.0005)
+contact_material.SetComplianceT(0.0005)
+
+# Create floor
+mfloor = chrono.ChBodyEasyBox(20, 0.3, 6, 100000,True,True, contact_material)
+mfloor.SetRot( chrono.ChQuaternionD(  -0.0610485, 0, 0, 0.9981348  )) # Tilt the floor -7 degrees
+mfloor.SetBodyFixed(True)
+mysystem.Add(mfloor)
+
+# Attach color asset
+mfloorcolor = chrono.ChColorAsset()
+mfloorcolor.SetColor(chrono.ChColor(0.3, 0.3, 0.6))
+mfloor.AddAsset(mfloorcolor)
+
+
+# ---------------------------------------------------------------------
+#
+#  Create an Irrlicht application to visualize the system
+
+myapplication = chronoirr.ChIrrApp(mysystem, 'PyChrono example', chronoirr.dimension2du(1024,768))
+
+myapplication.AddTypicalSky()
+myapplication.AddTypicalLogo(chrono.GetChronoDataFile('logo_pychrono_alpha.png'))
+myapplication.AddTypicalCamera(chronoirr.vector3df(0,5,-6), chronoirr.vector3df(0,0,0))
+myapplication.AddTypicalCamera(chronoirr.vector3df(0, 4, -6))
+myapplication.AddTypicalLights()
+
+myapplication.AssetBindAll();
+
+			# ==IMPORTANT!== Use this function for 'converting' into Irrlicht meshes the assets
+			# that you added to the bodies into 3D shapes, they can be visualized by Irrlicht!
+
+myapplication.AssetUpdateAll();
+
+            # If you want to show shadows because you used "AddLightWithShadow()'
+            # you must remember this:
+myapplication.AddShadowAll();
+
+
+# ---------------------------------------------------------------------
+#
+#  Run the simulation
+#
+
+
+myapplication.SetTimestep(0.005)
+
+while(myapplication.GetDevice().run()):
+    print(body_B.GetContactForce ())
+    myapplication.BeginScene()
+    myapplication.DrawAll()
+    myapplication.DoStep()
+    myapplication.EndScene()
+
+
