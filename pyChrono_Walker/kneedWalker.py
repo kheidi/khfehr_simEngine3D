b047467d6b6c0fe609dfad39968829290741dee2
Katherine Fehr <kfehr@wisc.edu>
2020-12-12 01:33:41 -0600

    Adds knee joints and separates shank and thigh

--- pyChrono_Walker/kneedWalker.py
+++ pyChrono_Walker/kneedWalker.py
@@ -20,7 +20,7 @@ import pandas as pd
 #  Create the simulation system and add items
 #
 
-incline = np.radians(1)
+incline = np.radians(3)
 mysystem  = chrono.ChSystemNSC()
 mysystem.Set_G_acc(chrono.ChVectorD(-9.810*np.sin(incline),-9.810*np.cos(incline),0))
 
@@ -44,7 +44,7 @@ contact_material.SetDampingF(0.2)
 
 # Create floor
 floor_thickness = 0.3
-mfloor = chrono.ChBodyEasyBox(20, floor_thickness, 6, 100000,True,True, contact_material)
+mfloor = chrono.ChBodyEasyBox(3, floor_thickness, 1, 100000,True,True, contact_material)
 mfloor.SetPos(chrono.ChVectorD(0, -floor_thickness/2, 0))
 # mfloor.SetRot( chrono.ChQuaternionD(  -0.0610485, 0, 0, 0.9981348  )) # Tilt the floor -7 degrees
 mfloor.SetBodyFixed(True)
@@ -71,40 +71,57 @@ mwall.AddAsset(mfloorcolor)
 
 # Define Properties
 
+fix = True
+#fix = False
+
 mymass = 10 #kg
-foot_mass = mymass*0.00133 #https://exrx.net/Kinesiology/Segments
+foot_mass = mymass*0.0133 #https://exrx.net/Kinesiology/Segments
 leg_mass = mymass*0.0535
 leg_mass = 0.01
 leg_length = .1
 leg_radius = 0.005
 leg_volume = math.pi*pow(leg_radius,2)*leg_length
 leg_density = leg_mass/leg_volume
-theta1 = np.radians(11.5)
-theta2 = np.radians(7)
+theta1 = np.radians(5)
+theta2 = np.radians(30)
 leg_inertia = chrono.ChVectorD(0.5*leg_mass*pow(leg_radius,2), (leg_mass/12)*(3*pow(leg_radius,2)+pow(leg_length,2)),(leg_mass/12)*(3*pow(leg_radius,2)+pow(leg_length,2)))
 foot_length = 0.005
 foot_radius = 0.03
 foot_volume = math.pi*pow(foot_radius,2)*foot_length
 foot_density = foot_mass/foot_volume
 
-initial_hipPos = chrono.ChVectorD(0,0.1,0)
+# Knee Properties
+thigh_mass = leg_mass/2
+thigh_length = leg_length/2
+thigh_radius = leg_radius
+thigh_volume = math.pi*pow(thigh_radius,2)*thigh_length
+thigh_density = thigh_mass/thigh_volume
+thigh_inertia = chrono.ChVectorD(0.5*thigh_mass*pow(thigh_radius,2), (thigh_mass/12)*(3*pow(thigh_radius,2)+pow(thigh_length,2)),(thigh_mass/12)*(3*pow(thigh_radius,2)+pow(thigh_length,2)))
+shank_mass = leg_mass/2
+shank_length = leg_length/2
+shank_radius = leg_radius
+shank_volume = math.pi*pow(shank_radius,2)*shank_length
+shank_density = shank_mass/shank_volume
+shank_inertia = chrono.ChVectorD(0.5*shank_mass*pow(shank_radius,2), (shank_mass/12)*(3*pow(shank_radius,2)+pow(shank_length,2)),(shank_mass/12)*(3*pow(shank_radius,2)+pow(shank_length,2)))
+
+
+initial_hipPos = chrono.ChVectorD(0,leg_length,0)
 print('hipPos',initial_hipPos)
 
 # Create hip
 hipBody = chrono.ChBodyEasyEllipsoid(chrono.ChVectorD(.02,.02,.02 ),leg_density,True,False,contact_material)
 hipBody.SetPos(initial_hipPos)
-# hipBody.SetBodyFixed(True)
 mysystem.Add(hipBody)
 
 # Hip Joint locations
 swing_hipJoint_P = chrono.ChMarker()
 h = hipBody.GetPos()
-swing_hipJoint_P.SetPos(chrono.ChVectorD(h.x,h.y,h.z+0.001))
+swing_hipJoint_P.SetPos(chrono.ChVectorD(h.x,h.y,h.z+0.01))
 hipBody.AddMarker(swing_hipJoint_P)
 print('swing hip', swing_hipJoint_P.GetPos())
 
 stance_hipJoint_P = chrono.ChMarker()
-stance_hipJoint_P.SetPos(chrono.ChVectorD(h.x,h.y,(h.z-0.001)))
+stance_hipJoint_P.SetPos(chrono.ChVectorD(h.x,h.y,(h.z-0.01)))
 hipBody.AddMarker(stance_hipJoint_P)
 print('stance hip', stance_hipJoint_P.GetPos())
 
@@ -115,36 +132,88 @@ swing_hip_frame.SetCoord(swing_hipJoint_P.GetCoord())
 stance_hip_frame = chrono.ChFrameD()
 stance_hip_frame.SetCoord(stance_hipJoint_P.GetCoord())
 
-# Create swing leg
-leg_swing = chrono.ChBodyEasyCylinder(leg_radius, leg_length, leg_density,True,False,contact_material)
-center_leg_swing= swing_hip_frame*chrono.ChVectorD(-(leg_length/2)*np.sin(-theta2),-(leg_length/2)*np.cos(-theta2),0)
-print(center_leg_swing)
+
+# Create swing thigh
+thigh_swing = chrono.ChBodyEasyCylinder(thigh_radius, thigh_length, thigh_density,True,False,contact_material)
+center_thigh_swing= swing_hip_frame*chrono.ChVectorD(-(thigh_length/2)*np.sin(-theta2),-(thigh_length/2)*np.cos(-theta2),0)
+print(center_thigh_swing)
 q = chrono.ChQuaternionD()
 q.Q_from_AngAxis(-theta2, chrono.ChVectorD(0,0,-1))
 print('q',q)
-leg_swing.SetPos(center_leg_swing)
-leg_swing.SetRot(q)
-# leg_swing.SetBodyFixed(True)
-mysystem.Add(leg_swing)
+thigh_swing.SetPos(center_thigh_swing)
+thigh_swing.SetRot(q)
+mysystem.Add(thigh_swing)
 
-# Create stance leg
-leg_stance = chrono.ChBodyEasyCylinder(leg_radius, leg_length, leg_density,True,False,contact_material)
-center_leg_stance= stance_hip_frame*chrono.ChVectorD(-(leg_length/2)*np.sin(theta1),-(leg_length/2)*np.cos(theta1),0)
-print(center_leg_stance)
+# Create stance thigh
+thigh_stance = chrono.ChBodyEasyCylinder(thigh_radius, thigh_length, thigh_density,True,False,contact_material)
+center_thigh_stance= stance_hip_frame*chrono.ChVectorD(-(thigh_length/2)*np.sin(theta1),-(thigh_length/2)*np.cos(theta1),0)
+print(center_thigh_stance)
 q = chrono.ChQuaternionD()
 q.Q_from_AngAxis((theta1), chrono.ChVectorD(0,0,-1))
 print('q',q)
-leg_stance.SetPos(center_leg_stance)
-leg_stance.SetRot(q)
-# leg_stance.SetBodyFixed(True)
-mysystem.Add(leg_stance)
+thigh_stance.SetPos(center_thigh_stance)
+thigh_stance.SetRot(q)
+mysystem.Add(thigh_stance)
+
+# knee Joint locations
+swing_kneeJoint_P = chrono.ChMarker()
+h = thigh_swing.GetPos()
+swing_kneeJoint_P.SetPos(swing_hip_frame*chrono.ChVectorD(-(thigh_length)*np.sin(-theta2),-(thigh_length)*np.cos(-theta2),0))
+thigh_swing.AddMarker(swing_kneeJoint_P)
+print('swing knee', swing_kneeJoint_P.GetPos())
+
+stance_kneeJoint_P = chrono.ChMarker()
+h = thigh_stance.GetPos()
+stance_kneeJoint_P.SetPos(stance_hip_frame*chrono.ChVectorD(-(thigh_length)*np.sin(theta1),-(thigh_length)*np.cos(theta1),0))
+thigh_stance.AddMarker(stance_kneeJoint_P)
+print('stance knee', stance_kneeJoint_P.GetPos())
+
+# Make a reference frame where the leg connects to the knee
+swing_knee_frame = chrono.ChFrameD()
+swing_knee_frame.SetCoord(swing_kneeJoint_P.GetCoord())
+
+stance_knee_frame = chrono.ChFrameD()
+stance_knee_frame.SetCoord(stance_kneeJoint_P.GetCoord())
+
+a = 15
+b = 35
+
+# Create swing shank
+shank_swing = chrono.ChBodyEasyCylinder(shank_radius, shank_length, shank_density,True,False,contact_material)
+center_shank_swing= swing_hip_frame*chrono.ChVectorD(-((3*shank_length)/2)*np.sin(-theta2-np.radians(a)),-((3*shank_length)/2)*np.cos(-theta2-np.radians(a)),0)
+print(center_shank_swing)
+q = chrono.ChQuaternionD()
+q.Q_from_AngAxis((-theta2-np.radians(b)), chrono.ChVectorD(0,0,-1))
+print('q',q)
+shank_swing.SetPos(center_shank_swing)
+shank_swing.SetRot(q)
+
+mysystem.Add(shank_swing)
+
+# Create stance shank
+shank_stance = chrono.ChBodyEasyCylinder(shank_radius, shank_length, shank_density,True,False,contact_material)
+center_shank_stance= stance_hip_frame*chrono.ChVectorD(-((3*shank_length)/2)*np.sin(theta1),-((3*shank_length)/2)*np.cos(theta1),0)
+print(center_shank_stance)
+q = chrono.ChQuaternionD()
+q.Q_from_AngAxis((theta1), chrono.ChVectorD(0,0,-1))
+print('q',q)
+shank_stance.SetPos(center_shank_stance)
+shank_stance.SetRot(q)
+mysystem.Add(shank_stance)
+
+if fix == True:
+    hipBody.SetBodyFixed(True)
+    thigh_stance.SetBodyFixed(True)
+    shank_stance.SetBodyFixed(True)
+    shank_swing.SetBodyFixed(True)
+    thigh_swing.SetBodyFixed(True)
 
 # Create feet
 foot_swing = chrono.ChBodyEasyCylinder(foot_radius, foot_length, foot_density,True,True,contact_material)
-end_leg_swing= swing_hip_frame*chrono.ChVectorD(-(leg_length-foot_radius+0.001)*np.sin(-theta2),-(leg_length-foot_radius+0.001)*np.cos(-theta2),0)
+end_leg_swing= swing_knee_frame*chrono.ChVectorD(-(shank_length-foot_radius)*np.sin(-theta2-np.radians(a)),-(shank_length-foot_radius)*np.cos(-theta2-np.radians(a)),0)
 print(end_leg_swing)
 q1 = chrono.ChQuaternionD()
-q1.Q_from_AngAxis(-theta2, chrono.ChVectorD(0,0,-1))
+q1.Q_from_AngAxis((-theta2-np.radians(b)), chrono.ChVectorD(0,0,-1))
 q2 = chrono.ChQuaternionD()
 q2.Q_from_AngAxis(np.radians(90), chrono.ChVectorD(1,0,0))
 print('q',q1*q2)
@@ -154,7 +223,7 @@ foot_swing.SetRot(q1*q2)
 mysystem.Add(foot_swing)
 
 foot_stance = chrono.ChBodyEasyCylinder(foot_radius, foot_length, foot_density,True,True,contact_material)
-end_leg_stance=  stance_hip_frame*chrono.ChVectorD(-(leg_length-foot_radius+0.001)*np.sin(theta1),-(leg_length-foot_radius+0.001)*np.cos(theta1),0)
+end_leg_stance=  stance_knee_frame*chrono.ChVectorD(-(shank_length-foot_radius)*np.sin(theta1),-(shank_length-foot_radius)*np.cos(theta1),0)
 print(end_leg_stance)
 q1 = chrono.ChQuaternionD()
 q1.Q_from_AngAxis(theta1, chrono.ChVectorD(0,0,-1))
@@ -175,7 +244,7 @@ mysystem.Add(foot_stance)
 
 Jprim1 = chrono.ChLinkMateParallel()
 
-Jprim1.Initialize(leg_stance,
+Jprim1.Initialize(thigh_stance,
                   mfloor,True,
                   chrono.ChVectorD(0,0,1),
                   chrono.ChVectorD(0,0,1),
@@ -184,17 +253,38 @@ Jprim1.Initialize(leg_stance,
 
 mysystem.AddLink(Jprim1)
 
+Jprim2 = chrono.ChLinkMateParallel()
+
+Jprim2.Initialize(shank_stance,
+                  mfloor,True,
+                  chrono.ChVectorD(0,0,1),
+                  chrono.ChVectorD(0,0,1),
+                  chrono.ChVectorD(0,0,1),
+                  chrono.ChVectorD(0,0,1))
+
+mysystem.AddLink(Jprim2)
+
 # Add a revolute joint where the leg connects to the hip 
 hipJoint_swing = chrono.ChLinkLockRevolute()
 hipJoint_swing.SetName('Revolute')
-hipJoint_swing.Initialize(leg_swing,hipBody,True,chrono.ChCoordsysD(chrono.ChVectorD(0,leg_length/2,0),chrono.ChQuaternionD(1, 0, 0, 0)),chrono.ChCoordsysD(chrono.ChVectorD(0,0,0.01),chrono.ChQuaternionD(1, 0, 0, 0)))
+hipJoint_swing.Initialize(thigh_swing,hipBody,True,chrono.ChCoordsysD(chrono.ChVectorD(0,thigh_length/2,0),chrono.ChQuaternionD(1, 0, 0, 0)),chrono.ChCoordsysD(chrono.ChVectorD(0,0,0.01),chrono.ChQuaternionD(1, 0, 0, 0)))
 mysystem.AddLink(hipJoint_swing)
 
 hipJoint_stance = chrono.ChLinkLockRevolute()
 hipJoint_stance.SetName('Revolute2')
-hipJoint_stance.Initialize(leg_stance,hipBody,True,chrono.ChCoordsysD(chrono.ChVectorD(0,leg_length/2,0),chrono.ChQuaternionD(1, 0, 0, 0)),chrono.ChCoordsysD(chrono.ChVectorD(0,0,-0.01),chrono.ChQuaternionD(1, 0, 0, 0)))
+hipJoint_stance.Initialize(thigh_stance,hipBody,True,chrono.ChCoordsysD(chrono.ChVectorD(0,thigh_length/2,0),chrono.ChQuaternionD(1, 0, 0, 0)),chrono.ChCoordsysD(chrono.ChVectorD(0,0,-0.01),chrono.ChQuaternionD(1, 0, 0, 0)))
 mysystem.AddLink(hipJoint_stance)
 
+# Add a revolute joint where the thigh connects to the shank
+kneeJoint_swing = chrono.ChLinkLockRevolute()
+kneeJoint_swing.SetName('Revolute')
+kneeJoint_swing.Initialize(shank_swing,thigh_swing,True,chrono.ChCoordsysD(chrono.ChVectorD(0,shank_length/2,0),chrono.ChQuaternionD(1, 0, 0, 0)),chrono.ChCoordsysD(chrono.ChVectorD(0,-thigh_length/2,0),chrono.ChQuaternionD(1, 0, 0, 0)))
+mysystem.AddLink(kneeJoint_swing)
+
+kneeJoint_stance = chrono.ChLinkLockRevolute()
+kneeJoint_stance.SetName('Revolute2')
+kneeJoint_stance.Initialize(shank_stance,thigh_stance,True,chrono.ChCoordsysD(chrono.ChVectorD(0,shank_length/2,0),chrono.ChQuaternionD(1, 0, 0, 0)),chrono.ChCoordsysD(chrono.ChVectorD(0,-thigh_length/2,0),chrono.ChQuaternionD(1, 0, 0, 0)))
+mysystem.AddLink(kneeJoint_stance)
 
 # Add some limits since we don't want the walker to do the splits
 
@@ -209,11 +299,11 @@ mysystem.AddLink(hipJoint_stance)
 # Constraint to fix the feet to the bottom of the legs
 
 ankleJoint_swing = chrono.ChLinkMateFix()
-ankleJoint_swing.Initialize(leg_swing, foot_swing)
+ankleJoint_swing.Initialize(shank_swing, foot_swing)
 mysystem.AddLink(ankleJoint_swing)
 
 ankleJoint_stance = chrono.ChLinkMateFix()
-ankleJoint_stance.Initialize(foot_stance, leg_stance)
+ankleJoint_stance.Initialize(foot_stance, shank_stance)
 mysystem.AddLink(ankleJoint_stance)
 
 # alignhip = chrono.ChLinkLockParallel()
@@ -297,9 +387,9 @@ while(myapplication.GetDevice().run()):
     myapplication.DrawAll()
     t = mysystem.GetChTime()
     print('time: ',mysystem.GetChTime())
-    stance_V = foot_stance.GetPos()-leg_stance.GetPos()
+    stance_V = thigh_stance.GetPos()-stance_hip_frame.GetPos()
     stance_V_norm = stance_V/(np.sqrt(pow(stance_V.x,2)+pow(stance_V.y,2)+pow(stance_V.z,2)))
-    swing_V = foot_swing.GetPos()-leg_swing.GetPos()
+    swing_V = thigh_swing.GetPos()-swing_hip_frame.GetPos()
     swing_V_norm = swing_V/(np.sqrt(pow(swing_V.x,2)+pow(swing_V.y,2)+pow(swing_V.z,2)))
     stance_array = np.array([stance_V_norm.x,stance_V_norm.y,stance_V_norm.z])
     swing_array = np.array([swing_V_norm.x,swing_V_norm.y,swing_V_norm.z])
